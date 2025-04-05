4afb42d5c517f4082fa070af4b810d7fe8c5bc7b
baruadb <barua700@gmail.com>
2025-04-05 01:28:39 -0400

    Fresh new committed

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,11 +4,11 @@ module "vpc" {
 
   name = "vprofile-eks"
 
-  cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr = "192.168.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 1, 3)
 
-  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+  private_subnets = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+  public_subnets  = ["192.168.4.0/24", "192.168.5.0/24", "192.168.6.0/24"]
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
